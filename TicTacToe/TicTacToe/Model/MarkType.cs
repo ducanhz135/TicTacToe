ee3dd03deb253045fa72b2f78fc87beeccfac825
duc anh <ducanhz135@gmail.com>
2020-04-07 21:53:13 +0700

    first commit

---
+++ TicTacToe/TicTacToe/Model/MarkType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Model
+{
+    public enum MarkType
+    {
+
+        /// <summary>
+        /// No Tick
+        /// </summary>
+        Free,
+        /// <summary>
+        /// O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// X
+        /// </summary>
+        Cross
+
+    }
+}
---
+++ TicTacToe/TicTacToe/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TicTacToe.ViewModel
+{
+    public class MainViewModel: BaseViewModel
+    {
+        public ICommand TickCommand { get; set; }
+
+        public MainViewModel()
+        {
+            TickCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                MessageBox.Show("Ok");
+            });
+        }
+    }
+}
