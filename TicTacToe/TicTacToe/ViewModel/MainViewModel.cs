ebd06972b7b85c6a40fd9598a619fa4d75726bef
duc anh <ducanhz135@gmail.com>
2020-04-09 21:59:57 +0700

    commit two way binding and optimise code

--- TicTacToe/TicTacToe/ViewModel/MainViewModel.cs
+++ TicTacToe/TicTacToe/ViewModel/MainViewModel.cs
@@ -10,70 +10,146 @@ namespace TicTacToe.ViewModel
 {
     public class MainViewModel: BaseViewModel
     {
-        /// <summary>
-        /// Holds the current results of cells in the active game
-        /// </summary>
+        string Cross = "X";
+        string Nought = "O";
+
         private MarkType[] results;
 
-        /// <summary>
-        /// True if it is player 1's turn (X) or player 2's turn (O)
-        /// </summary>
         private bool isPlayer1Turn;
 
-        /// <summary>
-        /// True if the game has ended
-        /// </summary>
         private bool isGameEnded;
 
+        private string _ButtonContent;
+        public string ButtonContent { get => _ButtonContent; set { _ButtonContent = value; OnPropertyChanged(); } }
+
+        private string _ButtonContent1;
+        public string ButtonContent1 { get => _ButtonContent1; set { _ButtonContent1 = value; OnPropertyChanged(); } }
+
+        private string _ButtonContent2;
+        public string ButtonContent2 { get => _ButtonContent2; set { _ButtonContent2 = value; OnPropertyChanged(); } }
+
+        private string _ButtonContent3;
+        public string ButtonContent3 { get => _ButtonContent3; set { _ButtonContent3 = value; OnPropertyChanged(); } }
+
+        private string _ButtonContent4;
+        public string ButtonContent4 { get => _ButtonContent4; set { _ButtonContent4 = value; OnPropertyChanged(); } }
+
+        private string _ButtonContent5;
+        public string ButtonContent5 { get => _ButtonContent5; set { _ButtonContent5 = value; OnPropertyChanged(); } }
+
+        private string _ButtonContent6;
+        public string ButtonContent6 { get => _ButtonContent6; set { _ButtonContent6 = value; OnPropertyChanged(); } }
+
+        private string _ButtonContent7;
+        public string ButtonContent7 { get => _ButtonContent7; set { _ButtonContent7 = value; OnPropertyChanged(); } }
+
+        private string _ButtonContent8;
+        public string ButtonContent8 { get => _ButtonContent8; set { _ButtonContent8 = value; OnPropertyChanged(); } }
 
         public ICommand TickCommand { get; set; }
+        public ICommand TickCommand1 { get; set; }
+        public ICommand TickCommand2 { get; set; }
+        public ICommand TickCommand3 { get; set; }
+        public ICommand TickCommand4 { get; set; }
+        public ICommand TickCommand5 { get; set; }
+        public ICommand TickCommand6 { get; set; }
+        public ICommand TickCommand7 { get; set; }
+        public ICommand TickCommand8 { get; set; }
+
         public ICommand LoadedWindowCommand { get; set; }
 
         public MainViewModel()
         {
             TickCommand = new RelayCommand<System.Windows.Controls.Button>((p) => { return true; }, (button) => {
 
-                
+                SetContentToResults(button);
 
-                // Find the buttons position in the array
-                var column = Grid.GetColumn(button);
-                var row = Grid.GetRow(button);
+                ButtonContent = isPlayer1Turn ? Cross : Nought;
 
-                var index = column + (row * 3);
-
-                // Don't do anything if the cell already has a value in it
-                if (results[index] != MarkType.Free)
-                    return;
-
-                // Set the cell value based on which players turn it is
-                results[index] = isPlayer1Turn ? MarkType.Cross : MarkType.Nought;
-
-                // Set button text to the result
-                button.Content = isPlayer1Turn ? "X" : "O";
-
-                // Change noughts to green
-                if (!isPlayer1Turn)
-                    button.Foreground = Brushes.Red;
-
-                // Check for a winner
-                var grid = button.Parent as Grid;
-                CheckForWinner(grid);
-
-                // show the winner
-                if (isGameEnded)
-                {
-                    MessageBox.Show((isPlayer1Turn ? "Player 1" : "Player 2" )+ " won the game");
-                }
-
-                // Toggle the players turns
-                isPlayer1Turn ^= true;
-
-                // Start a new game on the click after it finished
-                if (isGameEnded)
-                {
-                    NewGame(button.Parent as Grid);
-                }
+                DefineNextPlayerOrWinner();
             });
+
+            TickCommand1 = new RelayCommand<System.Windows.Controls.Button>((p) => { return true; }, (button) => {
+
+
+                SetContentToResults(button);
+
+                ButtonContent1 = isPlayer1Turn ? Cross : Nought;
+
+                DefineNextPlayerOrWinner();
+            });
+
+            TickCommand2 = new RelayCommand<System.Windows.Controls.Button>((p) => { return true; }, (button) => {
+
+
+                SetContentToResults(button);
+
+                ButtonContent2 = isPlayer1Turn ? Cross : Nought;
+
+                DefineNextPlayerOrWinner();
+            });
+
+            TickCommand3 = new RelayCommand<System.Windows.Controls.Button>((p) => { return true; }, (button) => {
+
+
+                SetContentToResults(button);
+
+                ButtonContent3 = isPlayer1Turn ? Cross : Nought;
+
+                DefineNextPlayerOrWinner();
+            });
+
+            TickCommand4 = new RelayCommand<System.Windows.Controls.Button>((p) => { return true; }, (button) => {
+
+
+                SetContentToResults(button);
+
+                ButtonContent4 = isPlayer1Turn ? Cross : Nought;
+
+                DefineNextPlayerOrWinner();
+            });
+
+            TickCommand5 = new RelayCommand<System.Windows.Controls.Button>((p) => { return true; }, (button) => {
+
+
+                SetContentToResults(button);
+
+                ButtonContent5 = isPlayer1Turn ? Cross : Nought;
+
+                DefineNextPlayerOrWinner();
+            });
+
+            TickCommand6 = new RelayCommand<System.Windows.Controls.Button>((p) => { return true; }, (button) => {
+
+
+                SetContentToResults(button);
+
+                ButtonContent6 = isPlayer1Turn ? Cross : Nought;
+
+                DefineNextPlayerOrWinner();
+            });
+
+            TickCommand7 = new RelayCommand<System.Windows.Controls.Button>((p) => { return true; }, (button) => {
+
+
+                SetContentToResults(button);
+
+                ButtonContent7 = isPlayer1Turn ? Cross : Nought;
+
+                DefineNextPlayerOrWinner();
+            });
+
+            TickCommand8 = new RelayCommand<System.Windows.Controls.Button>((p) => { return true; }, (button) => {
+
+
+                SetContentToResults(button);
+
+                ButtonContent8 = isPlayer1Turn ? Cross : Nought;
+
+                DefineNextPlayerOrWinner();
+            });
+
+
 
             LoadedWindowCommand = new RelayCommand<Grid>((p) => { return true; }, (p) =>
             {
@@ -81,179 +157,117 @@ namespace TicTacToe.ViewModel
                 if (p == null)
                     return;
 
-                FreeResults(p);
+                FreeResults();
 
             });
 
         }
 
-        /// <summary>
-        /// Starts a new game and clears all values back to the start
-        /// </summary>
-        private void NewGame(Grid grid)
+        private void NewGame()
         {
-            FreeResults(grid);
+            FreeResults();
 
-            // Make sure the game hasn't finished
             isGameEnded = false;
         }
 
-        private void FreeResults(Grid grid)
+
+        private void FreeResults()
         {
-            // Create a new blank array of free cells
             results = new MarkType[9];
 
             for (var i = 0; i < results.Length; i++)
                 results[i] = MarkType.Free;
 
-            // Make sure Player 1 starts the game
             isPlayer1Turn = true;
 
-            // Interate every button on the grid...
-            grid.Children.Cast<Button>().ToList().ForEach(button =>
-            {
-                // Change background, foreground and content to default values
-                button.Content = string.Empty;
-                button.Background = Brushes.White;
-                button.Foreground = Brushes.Blue;
-            });
+            ButtonContent = string.Empty;
+            ButtonContent1 = string.Empty;
+            ButtonContent2 = string.Empty;
+            ButtonContent3 = string.Empty;
+            ButtonContent4 = string.Empty;
+            ButtonContent5 = string.Empty;
+            ButtonContent6 = string.Empty;
+            ButtonContent7 = string.Empty;
+            ButtonContent8 = string.Empty;
+
         }
 
-        /// <summary>
-        /// Checks if there is a winner of a 3 line straight
-        /// </summary>
-        private void CheckForWinner(Grid grid)
+        private void CheckForWinner()
         {
-            var buttons = grid.Children.Cast<Button>().ToList();
 
-            //var btn = buttons.FirstOrDefault(x => x.Name == "Button0_0");
             #region Horizontal Wins
 
-            // Check for horizontal wins
-            //
-            //  - Row 0
-            //
-            if (results[0] != MarkType.Free && (results[0] & results[1] & results[2]) == results[0])
+            if (IsGameWon())
             {
-                // Game ends
                 isGameEnded = true;
-
-                // Highlight winning cells in green
-                GetButtonByName(buttons, "Button0_0").Background = GetButtonByName(buttons, "Button1_0").Background = GetButtonByName(buttons, "Button2_0").Background = Brushes.Green;
                 
             }
 
-            //
-            //  - Row 1
-            //
-            if (results[3] != MarkType.Free && (results[3] & results[4] & results[5]) == results[3])
+
+            if (IsGameWon())
             {
-                // Game ends
                 isGameEnded = true;
 
-                // Highlight winning cells in green
-                
-                GetButtonByName(buttons, "Button0_1").Background = GetButtonByName(buttons, "Button1_1").Background = GetButtonByName(buttons, "Button2_1").Background = Brushes.Green;
             }
-            //
-            //  - Row 2
-            //
-            if (results[6] != MarkType.Free && (results[6] & results[7] & results[8]) == results[6])
+
+            if (IsGameWon())
             {
-                // Game ends
                 isGameEnded = true;
 
-                // Highlight winning cells in green
-                
-                GetButtonByName(buttons, "Button0_2").Background = GetButtonByName(buttons, "Button1_2").Background = GetButtonByName(buttons, "Button2_2").Background = Brushes.Green;
             }
 
             #endregion
 
             #region Vertical Wins
 
-            // Check for vertical wins
-            //
-            //  - Column 0
-            //
-            if (results[0] != MarkType.Free && (results[0] & results[3] & results[6]) == results[0])
+            if (IsGameWon())
             {
-                // Game ends
+
                 isGameEnded = true;
 
-                // Highlight winning cells in green
-                GetButtonByName(buttons, "Button0_0").Background = GetButtonByName(buttons, "Button0_1").Background = GetButtonByName(buttons, "Button0_2").Background = Brushes.Green;
             }
-            //
-            //  - Column 1
-            //
-            if (results[1] != MarkType.Free && (results[1] & results[4] & results[7]) == results[1])
-            {
-                // Game ends
-                isGameEnded = true;
 
-                // Highlight winning cells in green
+            if (IsGameWon())
+            {
+
+                isGameEnded = true;
                 
-                GetButtonByName(buttons, "Button1_0").Background = GetButtonByName(buttons, "Button1_1").Background = GetButtonByName(buttons, "Button1_2").Background = Brushes.Green;
             }
-            //
-            //  - Column 2
-            //
-            if (results[2] != MarkType.Free && (results[2] & results[5] & results[8]) == results[2])
-            {
-                // Game ends
-                isGameEnded = true;
 
-                // Highlight winning cells in green
+            if (IsGameWon())
+            {
+
+                isGameEnded = true;
                 
-                GetButtonByName(buttons, "Button2_0").Background = GetButtonByName(buttons, "Button2_1").Background = GetButtonByName(buttons, "Button2_2").Background = Brushes.Green;
             }
 
             #endregion
 
             #region Diagonal Wins
 
-            // Check for diagonal wins
-            //
-            //  - Top Left Bottom Right
-            //
-            if (results[0] != MarkType.Free && (results[0] & results[4] & results[8]) == results[0])
+            if (IsGameWon())
             {
-                // Game ends
+
                 isGameEnded = true;
 
-                // Highlight winning cells in green
-                
-                GetButtonByName(buttons, "Button0_0").Background = GetButtonByName(buttons, "Button1_1").Background = GetButtonByName(buttons, "Button2_2").Background = Brushes.Green;
             }
-            //
-            //  - Top Right Bottom Left
-            //
-            if (results[2] != MarkType.Free && (results[2] & results[4] & results[6]) == results[2])
+
+            if (IsGameWon())
             {
-                // Game ends
                 isGameEnded = true;
 
-                // Highlight winning cells in green
-                
-                GetButtonByName(buttons, "Button2_0").Background = GetButtonByName(buttons, "Button1_1").Background = GetButtonByName(buttons, "Button0_2").Background = Brushes.Green;
             }
 
             #endregion
 
             #region No Winners
 
-            // Check for no winner and full board
+
             if (!results.Any(f => f == MarkType.Free))
             {
-                // Game ended
+
                 isGameEnded = true;
 
-                // Turn all cells orange
-                buttons.ForEach(button =>
-                {
-                    button.Background = Brushes.Orange;
-                });
             }
 
             #endregion
@@ -261,9 +275,47 @@ namespace TicTacToe.ViewModel
             
         }
 
-        private Button GetButtonByName(IList<Button> buttons, string name)
+        private void SetContentToResults(Button button)
         {
-            return buttons.FirstOrDefault(x => x.Name == name);
+            var column = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+
+            var index = column + (row * 3);
+
+            if (results[index] != MarkType.Free)
+                return;
+
+            results[index] = isPlayer1Turn ? MarkType.Cross : MarkType.Nought;
+        }
+
+        private void DefineNextPlayerOrWinner()
+        {
+
+            CheckForWinner();
+
+            if (isGameEnded)
+            {
+                MessageBox.Show((isPlayer1Turn ? "Player 1" : "Player 2") + " won the game");
+            }
+
+            isPlayer1Turn ^= true;
+
+            if (isGameEnded)
+            {
+                NewGame();
+            }
+        }
+
+        private bool IsGameWon()
+        {
+            return (results[0] != MarkType.Free && (results[0] & results[1] & results[2]) == results[0])
+                || (results[3] != MarkType.Free && (results[3] & results[4] & results[5]) == results[3])
+                || (results[6] != MarkType.Free && (results[6] & results[7] & results[8]) == results[6])
+                || (results[0] != MarkType.Free && (results[0] & results[3] & results[6]) == results[0])
+                || (results[1] != MarkType.Free && (results[1] & results[4] & results[7]) == results[1])
+                || (results[2] != MarkType.Free && (results[2] & results[5] & results[8]) == results[2])
+                || (results[0] != MarkType.Free && (results[0] & results[4] & results[8]) == results[0])
+                || (results[2] != MarkType.Free && (results[2] & results[4] & results[6]) == results[2]);
         }
     }
 }
